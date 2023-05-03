6d7870cef630d518b3fd5d666953e5b0200c4470
esperanzav <vasquezespe1990@gmail.com>
2023-05-02 22:56:01 -0400

    fist-commit

---
+++ acces/js.js
@@ -0,0 +1,7 @@
+//window.onscroll = function () {
+    //var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    //var elemento1 = document.getElementById("img_parallax");
+
+
+    //elemento1.style.bottom = posicion * 0.1 + "px";
+//}
